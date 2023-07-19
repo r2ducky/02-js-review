992156d752616820c7823af7698f016e7eb9a33d
Raghav <rsharma@duck.com>
2023-07-19 02:34:48 -0500

    CONCEPT Arrow Functions

--- starter/script.js
+++ starter/script.js
@@ -181,11 +181,14 @@ const updatedBook = {
 };
 updatedBook;
 
+// Arrow functions
+const getYear = (str) => str.split("-")[0];
+
 // Example of Template Literal
 // kinda like fstrings in python, used so you can use (tilda)`${expression}`
-const summary = `${title}, a ${pages}-page long book, was written by ${author}, and published in ${
-  publicationDate.split("-")[0]
-}. The book has ${hasMovieAdaptation ? "" : "not"}been adapted as a movie.`;
+const summary = `${title}, a ${pages}-page long book, was written by ${author}, and published in ${getYear(
+  publicationDate
+)}. The book has ${hasMovieAdaptation ? "" : "not"}been adapted as a movie.`;
 summary;
 
 // Ternary Operator
